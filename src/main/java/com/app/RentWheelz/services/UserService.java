3c7a9ce1d3e8f94de458bd8d6857b894df17ead4
akshay-sri <sriakshay2611999@gmail.com>
2024-03-24 23:47:45 +0530

    user registration

--- src/main/java/com/app/RentWheelz/services/UserService.java
+++ src/main/java/com/app/RentWheelz/services/UserService.java
@@ -25,36 +25,41 @@ public class UserService {
 
     public ApiResponse registration(UserDto userDto) {
         logger.info("Inside UserService.registration");
-        List<String> validations = UserHelper.validateUser(userDto);
-        if (validations.isEmpty()) {
-            if (userRepository.findByUserName(userDto.getUserName()) == null && userRepository.findByEmail(userDto.getEmail()) == null) {
-                User user = this.modelMapper.map(userDto, User.class);
-                user.setUserName(userDto.getUserName());
-                user.setEmail(userDto.getEmail());
-                user.setGender(userDto.getGender());
-                user.setPassword(userDto.getPassword());
-                user.setProofId(userDto.getProofId());
-                user.setFirstName(userDto.getFirstName());
-                user.setLastName(userDto.getLastName());
-                user.setMobileNumber(userDto.getMobileNumber());
-                User savedUser = this.userRepository.save(user);
-                logger.info("Exit from UserService.registration:{}", savedUser);
-                return new ApiResponse("Success!", "User registered successfully",null);
-            } else {
-                User u1 = this.userRepository.findByUserName(userDto.getUserName());
-                User u2 = this.userRepository.findByEmail(userDto.getEmail());
-                if (u1 != null && u2 != null) {
-                    throw new BadRequestException("User with userName: " + u1.getUserName() + " and Email: " + u2.getEmail() + " already exists");
-                } else if (u1 != null) {
-                    throw new BadRequestException("User with userName: " + u1.getUserName() + " already exists");
-                } else if (u2 != null) {
-                    throw new BadRequestException("User with email: " + u2.getEmail() + " already exists");
+        try {
+            List<String> validations = UserHelper.validateUser(userDto);
+            if (validations.isEmpty()) {
+                if (userRepository.findByUserName(userDto.getUserName()) == null && userRepository.findByEmail(userDto.getEmail()) == null) {
+                    User user = this.modelMapper.map(userDto, User.class);
+                    user.setUserName(userDto.getUserName());
+                    user.setEmail(userDto.getEmail());
+                    user.setGender(userDto.getGender());
+                    user.setPassword(userDto.getPassword());
+                    user.setProofId(userDto.getProofId());
+                    user.setFirstName(userDto.getFirstName());
+                    user.setLastName(userDto.getLastName());
+                    user.setMobileNumber(userDto.getMobileNumber());
+                    User savedUser = this.userRepository.save(user);
+                    logger.info("Exit from UserService.registration:{}", savedUser);
+                    return new ApiResponse("Success!", "User registered successfully", null);
+                } else {
+                    User u1 = this.userRepository.findByUserName(userDto.getUserName());
+                    User u2 = this.userRepository.findByEmail(userDto.getEmail());
+                    if (u1 != null && u2 != null) {
+                        throw new BadRequestException("User with userName: " + u1.getUserName() + " and Email: " + u2.getEmail() + " already exists");
+                    } else if (u1 != null) {
+                        throw new BadRequestException("User with userName: " + u1.getUserName() + " already exists");
+                    } else if (u2 != null) {
+                        throw new BadRequestException("User with email: " + u2.getEmail() + " already exists");
+                    }
                 }
             }
+            ApiResponse response = new ApiResponse("Failure", "There are some violated fields", validations);
+            logger.info("Exit from UserService.registration:{}", response);
+            return response;
         }
-        ApiResponse response = new ApiResponse("Failure", "There are some violated fields",validations);
-        logger.info("Exit from UserService.registration:{}",response);
-        return response;
+        catch (NullPointerException ex){
+            throw new BadRequestException("Request body cannot be null");
+        }
     }
 
 }
